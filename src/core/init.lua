5eb590772a373c38c59f9be1cab1e063782b074e
Paulo Carbalone <psoreto@outlook.com>
2023-10-24 09:00:14 -0300

    core plugin

---
+++ src/core/init.lua
@@ -0,0 +1,5 @@
+--- @param app App
+return function(app)
+  app
+      :add_plugin('src.core.state')
+end
--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,5 @@
 local App = require 'lib.app'
 
 App()
--- :add_plugin('src.raylib')
--- :add_plugin('src.world')
-    :add_plugin('src.bunny')
+    :add_plugin('src.core')
     :run()
