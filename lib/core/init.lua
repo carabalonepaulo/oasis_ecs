a59fbd6ba1dedc6e2b6625955f1382622194563c
Paulo Carbalone <psoreto@outlook.com>
2023-10-29 09:58:34 -0300

    asset server plugin loaded on core

--- lib/core/init.lua
+++ lib/core/init.lua
@@ -1,6 +1,9 @@
+local asset_server_plugin = require('lib.core.asset_server').plugin
+
 --- @param app App
 return function(app)
   app
+      :add_plugin(asset_server_plugin)
       :add_plugin('lib.core.state')
       :add_plugin('lib.core.window')
       :add_plugin('lib.core.renderer')
