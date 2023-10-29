f0977fa38edda422bf985d31632b692a473b96b2
Paulo Carbalone <psoreto@outlook.com>
2023-10-29 09:58:17 -0300

    asset server has cache (?) and may notify the world

--- lib/core/asset_server.lua
+++ lib/core/asset_server.lua
@@ -1,7 +1,10 @@
 local Slab = require 'lib.slab'
+local Queue = require 'lib.queue'
+
 local textures = Slab(1024)
 local handlers = {}
 local mirror = {}
+local events = Queue()
 
 local rl = require 'lib.raylib'
 local load_texture = rl.LoadTexture
@@ -16,11 +19,16 @@ end
 --- @param path string
 --- @return integer
 local function load(path)
+  if mirror[path] then
+    return mirror[path]
+  end
+
   local ext = path:get_file_extension()
   assert(handlers[ext], "Can't load this kind of file.")
 
   local id = textures:insert(handlers[ext]('./assets/' .. path))
   mirror[path] = id
+  events:enqueue({ 'texture_loaded', id })
   return id
 end
 
@@ -35,7 +43,22 @@ register_handler('png', function(path) --- @param path string
   return texture
 end)
 
+--- @param world World
+local function notify(world)
+  for event_data in events:iter() do
+    world:emit('asset_server', unpack(event_data))
+  end
+end
+
+--- @param app App
+local function plugin(app)
+  app
+      :add_event_domain('asset_server')
+      :add_system(notify, 'always')
+end
+
 return {
+  plugin = plugin,
   load = load,
   register_handler = register_handler,
   get_handle_value = get_handle_value
