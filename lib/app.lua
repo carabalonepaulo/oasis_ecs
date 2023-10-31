4921b926027d5f1f7114ba45dec05a73189d28d1
Paulo Carbalone <psoreto@outlook.com>
2023-10-31 07:31:19 -0300

    App.run extracted into a separated file

--- lib/app.lua
+++ lib/app.lua
@@ -5,9 +5,7 @@ require 'ext.table'
 
 local Object = require 'vendor.object'
 local Array = require 'lib.array'
-local Slab = require 'lib.slab'
-local Queue = require 'lib.queue'
-local printf = function(...) print(string.format(...)) end
+local main = require 'lib.main'
 
 local Registry = require 'lib.registry'
 local World = require 'lib.world'
@@ -94,89 +92,7 @@ function App:add_event_domain(domain_name)
 end
 
 function App:run()
-  local world = self.world
-  local dispatch_table = {}
-  local when = self.qualified.when
-  local when_len = #when
-
-  self._known_domains:each(function(domain_name)
-    dispatch_table[domain_name] = {}
-  end)
-
-  for i = 1, when_len do
-    local ev_func = when[i][1]
-    local ev_name, ev_group
-    local cond = when[i][3]
-
-    local parts = when[i][2]:split('%.')
-    local parts_len = #parts
-
-    if parts_len == 1 then
-      ev_group = '__default'
-      ev_name = when[i][2]
-    elseif parts_len == 2 then
-      ev_group = parts[1]
-      ev_name = parts[2]
-    else
-      error(string.format('Invalid event identifier: "%s"!', when[i][2]))
-    end
-
-    if not dispatch_table[ev_group] then
-      dispatch_table[ev_group] = {}
-    end
-
-    if not dispatch_table[ev_group][ev_name] then
-      dispatch_table[ev_group][ev_name] = Array()
-    end
-
-    dispatch_table[ev_group][ev_name]:insert({ ev_func, cond })
-  end
-
-  local function execute_system(system, cond_list, ...)
-    local len = #cond_list
-    if len > 0 then
-      local can_execute = true
-      for i = 1, len do
-        if not cond_list[i](world) then
-          can_execute = false
-          break
-        end
-      end
-      if can_execute then
-        system(world, ...)
-      end
-    else
-      system(world, ...)
-    end
-  end
-
-  local once = self.qualified.once
-  local once_len = #once
-  local always = self.qualified.always
-  local always_len = #always
-
-  local meta
-
-  for i = 1, once_len do
-    meta = rawget(once, i)
-    execute_system(meta[1], meta[3])
-  end
-
-  while not world.should_quit do
-    for i = 1, always_len do
-      meta = rawget(always, i)
-      execute_system(meta[1], meta[3])
-    end
-
-    for event_data in world.events:iter() do
-      local callbacks = dispatch_table[event_data[1]][event_data[2]]
-      if callbacks then
-        for i = 1, #callbacks do
-          execute_system(callbacks[i][1], callbacks[i][2], unpack(event_data[3]))
-        end
-      end
-    end
-  end
+  main(self.world, self.qualified, self._known_domains)
 end
 
 return App
---
+++ lib/main.lua
@@ -0,0 +1,91 @@
+local Array = require 'lib.array'
+
+local function create_dispatch_table(known_domains, when)
+  local dispatch_table = {}
+  local when_len = #when
+
+  known_domains:each(function(domain_name)
+    dispatch_table[domain_name] = {}
+  end)
+
+  for i = 1, when_len do
+    local ev_func = when[i][1]
+    local ev_name, ev_group
+    local cond = when[i][3]
+
+    local parts = when[i][2]:split('%.')
+    local parts_len = #parts
+
+    if parts_len == 1 then
+      ev_group = '__default'
+      ev_name = when[i][2]
+    elseif parts_len == 2 then
+      ev_group = parts[1]
+      ev_name = parts[2]
+    else
+      error(string.format('Invalid event identifier: "%s"!', when[i][2]))
+    end
+
+    if not dispatch_table[ev_group] then
+      dispatch_table[ev_group] = {}
+    end
+
+    if not dispatch_table[ev_group][ev_name] then
+      dispatch_table[ev_group][ev_name] = Array()
+    end
+
+    dispatch_table[ev_group][ev_name]:insert({ ev_func, cond })
+  end
+
+  return dispatch_table
+end
+
+local function execute_system(world, system, cond_list, ...)
+  local len = #cond_list
+  if len > 0 then
+    local can_execute = true
+    for i = 1, len do
+      if not cond_list[i](world) then
+        can_execute = false
+        break
+      end
+    end
+    if can_execute then
+      system(world, ...)
+    end
+  else
+    system(world, ...)
+  end
+end
+
+return function(world, qualified, known_domains)
+  local dispatch_table = create_dispatch_table(known_domains, qualified.when)
+
+  local once = qualified.once
+  local once_len = #once
+  local always = qualified.always
+  local always_len = #always
+
+  local meta
+
+  for i = 1, once_len do
+    meta = rawget(once, i)
+    execute_system(world, meta[1], meta[3])
+  end
+
+  while not world.should_quit do
+    for i = 1, always_len do
+      meta = rawget(always, i)
+      execute_system(world, meta[1], meta[3])
+    end
+
+    for event_data in world.events:iter() do
+      local callbacks = dispatch_table[event_data[1]][event_data[2]]
+      if callbacks then
+        for i = 1, #callbacks do
+          execute_system(world, callbacks[i][1], callbacks[i][2], unpack(event_data[3]))
+        end
+      end
+    end
+  end
+end
